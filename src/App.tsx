88e3ada66621c4a740a832f7cf39bda2a784c7aa
Francis Lagares <francis.lagares@gmail.com>
2021-12-22 20:46:54 +0100

    feat: create Home, About & NotFound routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from 'components/layout/Footer';
 import Navbar from 'components/layout/Navbar';
+import About from 'pages/About';
+import Home from 'pages/Home';
+import NotFound from 'pages/NotFound';
+
 import './styles/index.css';
 
 const App = () => {
@@ -9,7 +13,14 @@ const App = () => {
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main className='container mx-auto px-3 pb-12'>Content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
