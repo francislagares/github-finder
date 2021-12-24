232411eb06d0f434be8d4da91c83fb82dc751252
Francis Lagares <francis.lagares@gmail.com>
2021-12-24 21:01:58 +0100

    feat: define action types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -3,3 +3,24 @@ interface IUser {
   login: string;
   avatar_url: string;
 }
+
+interface IState {
+  users: IUser[];
+  loading: boolean;
+}
+
+enum ActionType {
+  GET_USERS = 'GET_USERS',
+  SET_LOADING = 'SET_LOADING',
+}
+
+interface IGetUsers {
+  type: ActionType.GET_USERS;
+  payload: IUser[];
+}
+
+interface ISetLoading {
+  type: ActionType.SET_LOADING;
+}
+
+type IAction = IGetUsers | ISetLoading;
